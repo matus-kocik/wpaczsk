85cbcc3a0f2198380d9dde129813aa4e02ffe531
Matúš Kočik <matuskocik@gmail.com>
2024-05-01 11:43:41 +0200

    create new app geography

--- wpaczskBE/wpaczskBE_projekt/settings.py
+++ wpaczskBE/wpaczskBE_projekt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv["SECRET_KEY"]
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     
     #My apps
     'taxonomy',
+    'geography',
 ]
 
 MIDDLEWARE = [
