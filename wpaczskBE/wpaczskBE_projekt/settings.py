cc02eed543c1fc270f8146b42afd439c92c0b958
Matúš Kočik <matuskocik@gmail.com>
2024-05-01 19:42:17 +0200

    small change

--- wpaczskBE/wpaczskBE_projekt/settings.py
+++ wpaczskBE/wpaczskBE_projekt/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
