141b94fe1104f8bbb44c8570cf71db174d5e36d1
Matúš Kočik <matuskocik@gmail.com>
2024-06-16 20:17:23 +0200

    add wpa.html, small change

--- wpaczskBE/main/urls.py
+++ wpaczskBE/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, WPAView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('wpa/', WPAView.as_view(), name='wpa'),
 ]
--- wpaczskBE/main/views.py
+++ wpaczskBE/main/views.py
@@ -2,10 +2,13 @@ from django.views.generic import TemplateView
 from media.models import Image
 
 class HomeView(TemplateView):
-    template_name = 'main/home.html'
+    template_name = "main/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         images= list(Image.objects.filter(card_item = True, template_name = "home"))
         context["card_images"] = images
         return context
+    
+class WPAView(TemplateView):
+    template_name = "main/wpa.html"
