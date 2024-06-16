e0a19cdad94d00c59dc6b848bc916e1c873ae5c0
Matúš Kočik <matuskocik@gmail.com>
2024-06-16 10:29:31 +0200

    change homeview

--- wpaczskBE/main/views.py
+++ wpaczskBE/main/views.py
@@ -4,10 +4,3 @@ import random
 
 class HomeView(TemplateView):
     template_name = 'main/home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        images = list(Image.objects.filter(carousel=True))
-        random.shuffle(images)
-        context['carousel_images'] = images
-        return context
