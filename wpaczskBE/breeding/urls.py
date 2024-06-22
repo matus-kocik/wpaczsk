080b16b4a6d767f1b98c272786bce61b5adb5c72
Matúš Kočik <matuskocik@gmail.com>
2024-06-22 16:09:42 +0200

    create template breeding.html

--- wpaczskBE/breeding/urls.py
+++ wpaczskBE/breeding/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectsView
+from .views import ProjectsView, BreedingView
 
 urlpatterns = [
     path('projects/', ProjectsView.as_view(), name='projects'),
+    path('breeding/', BreedingView.as_view(), name ='breeding'),
 ]
--- wpaczskBE/breeding/views.py
+++ wpaczskBE/breeding/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class ProjectsView(TemplateView):
     template_name = "breeding/projects.html"
+    
+class BreedingView(TemplateView):
+    template_name = "breeding/breeding.html"
