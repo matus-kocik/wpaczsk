4aed9eb458863a9da4184947c56732ceb96b9b83
Matúš Kočik <matuskocik@gmail.com>
2024-06-23 18:47:48 +0200

    update articles template

--- wpaczskBE/content/views.py
+++ wpaczskBE/content/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from media.models import Image
+from content.models import Article
+
 
 class ArticlesView(TemplateView):
     template_name = "content/articles.html"
+    extra_context = {
+        "home_page_card_images": Image.objects.filter(
+            card_item=True, template_name="home"
+        ),
+        "home_page_card_articles": Article.objects.all().order_by("-publication_date"),
+    }
