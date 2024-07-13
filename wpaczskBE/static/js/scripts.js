d6b9b221509cb40573440f5c5603668774624630
Matúš Kočik <matuskocik@gmail.com>
2024-07-13 15:04:48 +0200

    small change with login and register

--- wpaczskBE/static/js/scripts.js
+++ wpaczskBE/static/js/scripts.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("currentYear").textContent = new Date().getFullYear();
 });
+
+function showRegisterForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+}
+
+function showLoginForm() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
